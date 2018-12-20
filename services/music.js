11a410530eeb673087e841beeee5cd7ac4fb87f3
chirva-ivan <pooeblos@gmail.com>
2018-12-20 14:37:40 +0500

    fix request params

--- services/music.js
+++ services/music.js
@@ -2,21 +2,21 @@ const axios = require('axios');
 const rateYourMusicService = require('./rateYourMusic');
 
 function getAlbumsList(
-    year = 2018, genres = 'ambient'
+    year = '2018', genres = 'ambient'
 ) {
     const url = 'https://rateyourmusic.com/customchart';
     const params = {
         page: 1,
         include_child_genres_chk: 1,
         include_child_genres: 1,
-        genre_include :1,
+        genre_include : 1,
         chart_type: 'top',
         type: 'album',
         year,
         genres
     };
 
-    return axios.get(url, params).then((response) => {
+    return axios.get(url, { params }).then((response) => {
         return rateYourMusicService.parse(response.data);
     }).then((music) => console.log(music));
 }
