5d56f60f2ae516fcc58e2fb40365a39252257b92
chirva-ivan <pooeblos@gmail.com>
2018-12-23 13:20:12 +0500

    add session genre

--- telegram/index.js
+++ telegram/index.js
@@ -1,7 +1,11 @@
 const { Composer, Markup } = require('micro-bot');
+const session = require('telegraf/session');
+
 const musicService = require('../services/music');
 
 const bot = new Composer();
+
+bot.use(session());
 
 const AMBIENT = 'ambient';
 const ELECTRONIC = 'electronic';
@@ -9,39 +13,45 @@ const METAL = 'metal';
 const POST_PUNK = 'post-punk';
 
 bot.command('start', (ctx) =>
-    ctx.replyWithMarkdown('start message', Markup
+    ctx.replyWithMarkdown('Select music genre from list below or type any other', Markup
         .keyboard([[AMBIENT, ELECTRONIC, METAL, POST_PUNK]])
         .resize()
         .extra()
     )
 );
 
-bot.command('help', (ctx) => ctx.replyWithMarkdown('type any music tag for request'));
+bot.command('help', (ctx) => ctx.replyWithMarkdown('Type any music genre for request'));
 
 // TODO: refactor to telegram scenes
-bot.hears([AMBIENT, ELECTRONIC, METAL, POST_PUNK], (ctx) => {
+bot.on('text', (ctx) => {
+    // TODO: add genre validation
     const genre = ctx.update.message.text;
 
-    ctx.replyWithMarkdown('select year:', Markup
-        .keyboard([['2018', '2017']])
-        .resize()
-        .extra()
-    ).then(() => {
-        bot.hears(['2018', '2017'], (ctx) => {
-            const year = ctx.update.message.text;
+    ctx.session.genre = genre;
 
-            ctx.replyWithMarkdown(`loading some ${genre}...`);
+    const year = ctx.update.message.text;
 
-            musicService.getAlbumsList(year, genre).then((albums) => {
-                const markup = albums.map((item) => {
-                    return `<code>${item.author} - ${item.title}</code>\n<i>${item.date} | Rating: ${item.rating}</i>`
-                });
+    ctx.replyWithMarkdown(`loading some ${genre}...`);
 
-                ctx.replyWithHTML(markup.join('\n\n'));
+    musicService.getAlbumsList(year, genre).then((albums) => {
+        const itemMarkups = albums.map((item) => getAlbumsListItemMarkups(item));
+        ctx.replyWithHTML(itemMarkups.join('\n\n')).then(() => {
+            ctx.replyWithMarkdown('Would you like to subscribe for updates', Markup
+                .keyboard([['Y', 'N']])
+                .resize()
+                .extra()
+            );
+
+            bot.hears(['Y', 'N'], (ctx) => {
+                // TODO: add subscription
             })
         });
-    });
+    })
 });
+
+function getAlbumsListItemMarkups(item) {
+    return `<code>${item.author} - ${item.title}</code>\n<i>${item.date} | Rating: ${item.rating}</i>`;
+}
 
 
 
